c32532c777e9f957480a4a99812b0c3d2415cc90
ohhmydeezy <dylang21@hotmail.co.uk>
2024-05-25 21:33:43 +0100

    removed placeholders items and added proper functionality

--- shoppinglistUI/src/app/shop/shoppinglist/shoppinglist.component.ts
+++ shoppinglistUI/src/app/shop/shoppinglist/shoppinglist.component.ts
@@ -8,12 +8,7 @@ import { Shoppinglist } from '../../models/shoppinglist.model';
 })
 export class ShoppinglistComponent implements OnInit {
 
-  shoppinglist: Shoppinglist[] = [
-    {item: 'Milk', quantity: 1},
-    {item: 'Bread', quantity: 2},
-    {item: 'Eggs', quantity: 12},
-    {item: 'Butter', quantity: 1},
-  ];
+  shoppinglist: Shoppinglist[] = [];
   constructor() { }
 
   ngOnInit(): void {
